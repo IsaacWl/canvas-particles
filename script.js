9a6e33b51dfbd696c6fe39342ac7aa8611541f2b
IsaacWL <isaacwls25@gmail.com>
2021-12-30 13:09:46 -0300

    canvas particles effect

---
+++ script.js
@@ -0,0 +1,89 @@
+onload = function() {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.background = '#005';
+    let particles = [];
+    
+    function randomIntFromRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+    function randomColor() {
+        return (Math.random() * 255) + 1;
+    }
+    class Particle {
+        constructor(x, y, radius, color, direction) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.color = color;
+            this.direction = direction;
+        }
+        draw() {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+        move() {
+            this.y += this.direction.y;
+            this.x += this.direction.x;
+            if (this.x + this.radius > canvas.width) 
+                this.direction.x -= Math.random();
+            if (this.y < 0)
+                this.direction.y = Math.random();
+            if (this.x < 0)
+                this.direction.x = Math.random();
+            if (this.y + this.radius > canvas.height)
+                this.direction.y -= Math.random();            
+        }
+        randomMove() {
+            this.direction.x = randomIntFromRange(-5, 5);
+            this.direction.y = randomIntFromRange(-5, 5);
+        }
+    }
+    function createParticles() {
+        for (let i = 0; i < 250; i++) {
+            const x = Math.random() * window.innerWidth;
+            const y = Math.random() * window.innerHeight;
+            const radius = (Math.random() * 2) + 1;
+            const color = `rgba(${randomColor()}, ${randomColor()}, ${randomColor(),.5})`;
+            particles.push(new Particle(x, y, radius, color, 
+                {x:Math.random() < 0.5?-1:1,y:Math.random() < 0.5?-1:1}));
+        }
+    }
+    createParticles();
+    function animation() {
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        particles.forEach(particle =>{
+            //particle.draw();
+            particle.move();
+        })
+        connect();
+        requestAnimationFrame(animation);
+    }
+    animation();
+    this.document.addEventListener('click', (e)=>{
+        particles.forEach(particle => {
+            particle.randomMove();
+        })
+    })
+    function connect() {
+        let opacity = 1;
+        for (let a = 0; a < particles.length; a++) {
+            for (let b = a; b < particles.length; b++) {
+                let distance = ((particles[a].x - particles[b].x) *(particles[a].x - particles[b].x)) + ((particles[a].y - particles[b].y)*(particles[a].y - particles[b].y))
+                if (distance < 700) {
+                    opacity = 1 - (distance /  10000);
+                    ctx.strokeStyle = 'rgba(255, 255, 255,'+ opacity + ')';
+                    ctx.beginPath();
+                    ctx.lineWidth = 1;
+                    ctx.moveTo(particles[a].x, particles[a].y);
+                    ctx.lineTo(particles[b].x, particles[b].y);
+                    ctx.stroke();
+                }
+            }
+        }
+    }
+}
